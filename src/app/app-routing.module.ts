c2e53f31a9694ccd2efa5d73ebd9160dfed71168
yemiwebby <yemiwebby@gmail.com>
2020-10-11 03:24:18 +0100

    Removed group view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GroupScreenComponent } from 'src/lib/cometchat-angular-ui-kit/src/lib/group-screen/group-screen.component';
-// import { GroupScreenComponent } from 'src/lib/cometchat-angular-ui-kit/src/lib/group-screen/group-screen.component';
 import { AuthComponent } from './auth/auth.component';
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
-import { GroupChatViewComponent } from './group-chat-view/group-chat-view.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CometchatAngularUiKitModule } from 'src/lib/cometchat-angular-ui-kit/src/lib/cometchat-angular-ui-kit.module';
-// import { CometchatAngularUiKitModule } from 'src/lib/cometchat-angular-ui-kit/src/lib/cometchat-angular-ui-kit.module';
 
 @NgModule({
-  declarations: [AppComponent, AuthComponent, GroupChatViewComponent],
+  declarations: [AppComponent, AuthComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
